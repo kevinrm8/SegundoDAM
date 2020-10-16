81b35fb70d56cef647ce538f78f7cad53e83384e
kevinrm8 <kevmar@alu.iesserpis.org>
2020-10-16 13:59:40 +0200

    16

---
+++ Acceso a datos/Proyectos/Ejer4OrientObj/src/ejer4orientobj/Electrodomestico.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer4orientobj;
+
+public class Electrodomestico {
+
+    private double precioBase;
+    private String color;
+    private char consumoEnergito;
+    private double peso;
+
+    public Electrodomestico() {
+        this.precioBase = 100;
+        this.color = "blanco";
+        this.consumoEnergito = 'F';
+        this.peso = 5;
+    }
+
+    public Electrodomestico(double precioBase, double peso) {
+        this.precioBase = precioBase;
+        this.color = "blanco";
+        this.consumoEnergito = 'F';
+        this.peso = peso;
+    }
+
+    public Electrodomestico(double precioBase, String color, char consumoEnergito, double peso) {
+        this.precioBase = precioBase;
+        this.color = comprobarColor(color);
+        this.consumoEnergito = comprobarConsumoEnergito(consumoEnergito);
+        this.peso = peso;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public char getConsumoEnergito() {
+        return consumoEnergito;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    private char comprobarConsumoEnergito(char letra) {
+
+        if (letra != 'A' && letra != 'B' && letra != 'C' && letra != 'D' && letra != 'E' && letra != 'F') {
+            return 'F';
+        } else {
+            return letra;
+        }
+
+    }
+
+    private String comprobarColor(String color) {
+
+        if (!color.equals("blanco") && !color.equals("negro") && !color.equals("rojo") && !color.equals("azul") && !color.equals("gris")) {
+            return "blanco";
+        }
+        return color;
+    }
+
+    public double precioFinal() {
+        double precio = 0;
+
+        if (consumoEnergito == 'A') {
+            precio += 100;
+        } else {
+            if (consumoEnergito == 'B') {
+                precio += 80;
+            } else {
+                if (consumoEnergito == 'C') {
+                    precio += 60;
+                } else {
+                    if (consumoEnergito == 'D') {
+                        precio += 50;
+                    }
+                    if (consumoEnergito == 'E') {
+                        precio += 30;
+                    } else {
+                        if (consumoEnergito == 'F') {
+                            precio += 10;
+                        }
+                    }
+                }
+            }
+        }
+
+        return precio;
+    }
+
+}
