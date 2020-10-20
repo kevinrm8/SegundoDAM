ed5ebc32c55a4881ebb70510b27bf689af0f9648
kevinrm8 <kevmar@alu.iesserpis.org>
2020-10-20 13:10:03 +0200

    ADSQL

---
+++ Acceso a datos/Tema 2/ProyectosT2/ManejarJDBC/src/main/java/datos/Conexion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author kevmar
+ */
+public class Conexion {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/TEST?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "accesodatos";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+
+    public static void close(ResultSet rs) throws SQLException {
+        rs.close();
+    }
+    public static void close (Statement smtm) throws SQLException{
+    smtm.close();
+    }
+    public static void close(Connection conn) throws SQLException{
+    conn.close();
+    }
+    
+}
---
+++ Acceso a datos/Tema 2/ProyectosT2/ManejarJDBC/src/main/java/domain/Persona.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author kevmar
+ */
+public class Persona {
+    private int id_persona;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona() {
+//        this.id_persona = id_persona;
+//        this.nombre = nombre;
+//        this.apellidos = apellidos;
+//        this.edad = edad;
+    }
+
+    public Persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+    
+    
+    
+    
+}
