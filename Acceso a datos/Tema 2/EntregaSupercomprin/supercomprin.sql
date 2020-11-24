0e57dbed1de77e71e1397cdf4c76d4da20f19a17
kevinrm8 <kevmar@alu.iesserpis.org>
2020-11-25 00:37:53 +0100

    sql

--- Acceso a datos/Tema 2/EntregaSupercomprin/supercomprin.sql
+++ Acceso a datos/Tema 2/EntregaSupercomprin/supercomprin.sql
@@ -18,21 +18,20 @@
 --
 -- Table structure for table `compra`
 --
-use supercomprin;
+
 DROP TABLE IF EXISTS `compra`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `compra` (
   `idCompra` int(11) NOT NULL AUTO_INCREMENT,
-  `FechaCompra` date NOT NULL,
-  `e-Wallet_id_e-Wallet` int(11) NOT NULL,
+  `eWallet_id_eWallet` int(11) NOT NULL,
   `Producto_idProducto` int(11) NOT NULL,
   PRIMARY KEY (`idCompra`),
-  KEY `fk_Compra_e-Wallet_idx` (`e-Wallet_id_e-Wallet`),
+  KEY `fk_Compra_e-Wallet_idx` (`eWallet_id_eWallet`),
   KEY `fk_Compra_Producto1_idx` (`Producto_idProducto`),
   CONSTRAINT `fk_Compra_Producto1` FOREIGN KEY (`Producto_idProducto`) REFERENCES `producto` (`idProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Compra_e-Wallet` FOREIGN KEY (`e-Wallet_id_e-Wallet`) REFERENCES `ewallet` (`id_eWallet`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_Compra_e-Wallet` FOREIGN KEY (`eWallet_id_eWallet`) REFERENCES `ewallet` (`id_eWallet`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +40,7 @@ CREATE TABLE `compra` (
 
 LOCK TABLES `compra` WRITE;
 /*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+INSERT INTO `compra` VALUES (1,2,1),(2,2,3),(3,2,5),(5,2,1),(8,2,1),(9,2,1),(11,2,1),(13,2,1);
 /*!40000 ALTER TABLE `compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,13 +53,13 @@ DROP TABLE IF EXISTS `devolucion`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `devolucion` (
   `idDevolucion` int(11) NOT NULL AUTO_INCREMENT,
-  `e-Wallet_id_e-Wallet` int(11) NOT NULL,
+  `eWallet_id_eWallet` int(11) NOT NULL,
   `Producto_idProducto` int(11) NOT NULL,
   PRIMARY KEY (`idDevolucion`),
-  KEY `fk_Devolucion_e-Wallet1_idx` (`e-Wallet_id_e-Wallet`),
+  KEY `fk_Devolucion_e-Wallet1_idx` (`eWallet_id_eWallet`),
   KEY `fk_Devolucion_Producto1_idx` (`Producto_idProducto`),
   CONSTRAINT `fk_Devolucion_Producto1` FOREIGN KEY (`Producto_idProducto`) REFERENCES `producto` (`idProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Devolucion_e-Wallet1` FOREIGN KEY (`e-Wallet_id_e-Wallet`) REFERENCES `ewallet` (`id_eWallet`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_Devolucion_e-Wallet1` FOREIGN KEY (`eWallet_id_eWallet`) REFERENCES `ewallet` (`id_eWallet`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -84,12 +84,13 @@ CREATE TABLE `ewallet` (
   `Nombre` varchar(45) NOT NULL,
   `DNI` varchar(45) NOT NULL,
   `Edad` varchar(45) NOT NULL,
-  `FechaNacimiento` varchar(45) NOT NULL,
+  `FechaNacimiento` date NOT NULL,
   `Telefono` int(11) NOT NULL,
   `Saldo` float NOT NULL,
+  `Puntos` float NOT NULL,
   PRIMARY KEY (`id_eWallet`),
   UNIQUE KEY `DNI_UNIQUE` (`DNI`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +99,7 @@ CREATE TABLE `ewallet` (
 
 LOCK TABLES `ewallet` WRITE;
 /*!40000 ALTER TABLE `ewallet` DISABLE KEYS */;
-INSERT INTO `ewallet` VALUES (1,'Kevin','355A','28','31/078/1992',56465456,1000);
+INSERT INTO `ewallet` VALUES (2,'NombreTest','DNITest','20','2000-07-30',1,71,20),(4,'NombreTest','DNITest1','20','2000-07-30',1,1000,0);
 /*!40000 ALTER TABLE `ewallet` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,10 +114,9 @@ CREATE TABLE `producto` (
   `idProducto` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Precio` float NOT NULL,
-  `Puntos` int(11) NOT NULL,
-  `Stock` int(11) NOT NULL,
+  `Puntos` float NOT NULL,
   PRIMARY KEY (`idProducto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +125,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` VALUES (1,'Monitor',100,10),(2,'Teclado Membrama',24.99,2),(3,'Teclado Mecanico',79.49,8),(4,'Raton',35,3),(5,'Auriculares',50,5),(6,'Placa Base 1.0',15,1),(7,'Placa Base 2.5 HDD',55,5),(8,'Alfombrilla',9.99,1),(9,'Microfono',134,13),(10,'WebCam HD',40,4),(11,'Webcam FULLHD',85,8),(12,'Altavoces 5.1',51,5),(13,'Altavoces 7.1',71,7);
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -137,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-18 21:36:12
+-- Dump completed on 2020-11-24 23:29:10
